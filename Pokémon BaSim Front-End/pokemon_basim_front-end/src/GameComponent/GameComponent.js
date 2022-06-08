98ab67498d16fff92ba7a798b49ee4b1f37815d9
i468162fhict <i468162@fhict.nl>
2022-06-08 12:02:19 +0200

    Created a neat looking game page

--- Pokémon BaSim Front-End/pokemon_basim_front-end/src/GameComponent/GameComponent.js
+++ Pokémon BaSim Front-End/pokemon_basim_front-end/src/GameComponent/GameComponent.js
@@ -9,18 +9,22 @@ class GameComponent extends React.Component {
     render() {
         return (
             <div className="Game-body">
-                <div className="App">
-                    <div className="App-top-text">Game Page</div>
-                </div>
                 <div className="Game-game">
-                    <div className="Game-visuals">
-
-                    </div>
-                    <div className="Game-moves">
-
+                    <div className="Game-area">
+                        <div className="Game-visuals">
+                            <img className="p1-pokemon" src={process.env.PUBLIC_URL+"Assets/arceus-back.gif"} alt="Arceus-back"/>
+                            <img className="p2-pokemon" src={process.env.PUBLIC_URL+"Assets/arceus-front.gif"} alt="Arceus-front"/>
+                        </div>
+                        <div className="Game-moves">
+                            <div className="action-btn atk-btn">Attack</div>
+                            <div className="action-btn item-btn">Item</div>
+                            <div className="action-btn quit-btn">Quit</div>
+                        </div>
                     </div>
                     <div className="Game-log">
-
+                        <div className="log-msg-box">
+                            <div>Test</div>
+                        </div>
                     </div>
                 </div>
             </div>
